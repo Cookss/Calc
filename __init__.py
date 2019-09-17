477f52dddf862f58dc2345dc11a829628d63c7dc
Cookss <54788058+Cookss@users.noreply.github.com>
2019-09-16 20:03:02 -0700

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -2,22 +2,20 @@
 vel = input("Type in the velocity: ")
 acc = input("Type in acceleration: ")
 time = input("Type in time: ")
-x = input("Type in x: ")
-xo = input("Type in xo: ")
 
 if vel == '?':
-    answer = vel
-    answer = int(vel) / int(acc)
+    answer = int(acc) / int(time)
     print('Velocity: ')
     print(answer)
 
+else:
+    x = input("Type in x: ")
+    xo = input("Type in xo: ")
 
-elif x == '?':
-    answer = x
-    answer = int(xo) + int(vel) * int(xo) * int(time) * int(.5) * int(acc) * int(time) ^ int(2)
+if x == '?':
+    answer1 = int(xo) + int(vel) * int(xo) * int(time) * int(.5) * int(acc) * int(time) ^ int(2)
     print('x: ')
-    print(answer)
-
+    print(answer1)
 
 
 
