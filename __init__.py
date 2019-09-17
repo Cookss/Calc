b6e4145b91e0dc7e704fe612c01eb72ccd979e81
Cookss <54788058+Cookss@users.noreply.github.com>
2019-09-16 20:42:49 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,18 +1,27 @@
 vel = input("Type in the velocity: ")
-acc = input("Type in acceleration: ")
+speed = input("Type in speed: ")
 time = input("Type in time: ")
 
 if vel == '?':
-    answer = int(acc) / int(time)
+    answer = int(speed) / int(time)
+    vel = answer
     print('Velocity: ')
     print(answer)
-    raise SystemExit
+    x = input("Type in x (displacement): ")
+    xo = input("Type in xo (average displacement): ")
+
+    if x == '?':
+        answer1 = int(xo) + int(vel) * int(xo) * int(time) * int(.5) * int(speed) * int(time) ^ int(2)
+        print('x: ')
+        print(answer1)
+        raise SystemExit
+
 
 else:
-    x = input("Type in x: ")
-    xo = input("Type in xo: ")
+    x = input("Type in x (displacement): ")
+    xo = input("Type in xo (average displacement): ")
 
 if x == '?':
-    answer1 = int(xo) + int(vel) * int(xo) * int(time) * int(.5) * int(acc) * int(time) ^ int(2)
+    answer1 = int(xo) + int(vel) * int(xo) * int(time) * int(.5) * int(speed) * int(time) ^ int(2)
     print('x: ')
     print(answer1)
