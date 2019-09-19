8d060ffc4cd92e1fba4cb5705239d1942afd0777
Cookss <54788058+Cookss@users.noreply.github.com>
2019-09-18 21:02:34 -0700

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,49 @@
+vel = input("Type in the velocity: ")
+speed = input("Type in speed: ")
+time = input("Type in time: ")
+
+if vel == '?':
+    answer = int(speed) / int(time)
+    vel = answer
+    print('Velocity: ')
+    print(answer)
+    x = input("Type in x (displacement): ")
+    xo = input("Type in xo (average displacement): ")
+
+    if x == '?':
+        answer1 = int(xo) + int(vel) * int(xo) * int(time) * int(.5) * int(speed) * int(time) ^ int(2)
+        print('x: ')
+        print(answer1)
+        raise SystemExit
+
+
+    else:
+        x = input("Type in x (displacement): ")
+        xo = input("Type in xo (final displacement): ")
+
+    if x == '?':
+        answer1 = int(xo) + int(vel) * int(xo) * int(time) * int(.5) * int(speed) * int(time) ^ int(2)
+        print('x: ')
+        print(answer1)
+elif speed == '?':
+    speed = int(vel) * int(time)
+    print('Speed: ')
+    print(speed)
+    x = input("Type in x (displacement): ")
+    xo = input("Type in xo (average displacement): ")
+
+    if x == '?':
+        answer1 = int(xo) + int(vel) * int(xo) * int(time) * int(.5) * int(speed) * int(time) ^ int(2)
+        print('x: ')
+        print(answer1)
+        raise SystemExit
+
+
+    else:
+        x = input("Type in x (displacement): ")
+        xo = input("Type in xo (final displacement): ")
+
+    if x == '?':
+        answer1 = int(xo) + int(vel) * int(xo) * int(time) * int(.5) * int(speed) * int(time) ^ int(2)
+        print('x: ')
+        print(answer1)
